a5261f00585cecf9bc1856f9ae6628eb27f8fff7
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-11-18 04:48:59 -0500

    feat: implements put at employeeRouter

--- routers/employeeRouter.js
+++ routers/employeeRouter.js
@@ -33,6 +33,19 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.put('/:id', async(req, res) => {
+    try {
+        const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!employee) {
+            return res.status(404).json({ message: "Employee not found" })
+        }
+        res.status(201).json({ message: "Employee updated successfully", employee: employee })
+    } catch(error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
 
 
 module.exports = router
