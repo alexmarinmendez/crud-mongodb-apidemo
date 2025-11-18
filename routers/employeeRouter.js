1aa335863ff1069fab1babd15f00753906bb4e61
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-11-18 04:47:22 -0500

    feat: implements get by id at employeeRouter

--- routers/employeeRouter.js
+++ routers/employeeRouter.js
@@ -11,6 +11,18 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.get('/:id', async(req, res) => {
+    try {
+        const employee = await Employee.findById(req.params.id)
+        if (!employee) {
+            return res.status(404).json({ message: "Employee not found" })
+        }
+        res.status(200).json(employee)
+    } catch(error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 router.post('/', async(req, res) => {
     try {
         const employee = new Employee(req.body)
