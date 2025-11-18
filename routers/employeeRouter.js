036282dcbfcd7f1b6f349ab2c9b28af26e6cdc39
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-11-18 04:50:50 -0500

    feat: implements delete at employeeRouter

--- routers/employeeRouter.js
+++ routers/employeeRouter.js
@@ -45,7 +45,16 @@ router.put('/:id', async(req, res) => {
     }
 })
 
-
-
+router.delete('/:id', async(req, res) => {
+    try {
+        const employee = await Employee.findByIdAndDelete(req.params.id)
+        if (!employee) {
+            return res.status(404).json({ message: "Employee not found" })
+        }
+        res.status(200).json({ message: "Employee deleted successfully", employee: employee })
+    } catch(error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 module.exports = router
